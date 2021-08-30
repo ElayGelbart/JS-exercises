d68dfafab4d0947e8150f4744f7c4089795e2e0b
ElayGelbart <elaygelbart@gmail.com>
2021-08-30 18:45:22 +0300

    Update GradesArry.js
    to match the mission

--- Cyber4s/JS/GradesArry.js
+++ Cyber4s/JS/GradesArry.js
@@ -1,11 +1,12 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
+let secondHalfGrades = grades.slice(Math.floor((grades.length)/2));
+secondHalfGrades.sort();
 grades.sort();
 let lowestGrade = grades[0]
 let highestGrade = grades[grades.length-1];
 let differenceGrade = highestGrade - lowestGrade;
 let middleGrade = grades[Math.floor((grades.length-1)/2)];
-let secondHalfGrades = grades.slice(Math.floor((grades.length-1)/2));
 console.log('Range: ' + differenceGrade);
 console.log('Median ' + middleGrade);
 console.log('Half Range: ' + (secondHalfGrades[secondHalfGrades.length-1]-secondHalfGrades[0]));
-console.log(grades);
+
