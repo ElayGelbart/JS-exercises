7d5dd9f50ced5e2d2d44e7eeee267d8a42da2faa
ElayGelbart <elaygelbart@gmail.com>
2021-08-30 18:48:47 +0300

    Update GradesArry.js
    for all numbers

--- Cyber4s/JS/GradesArry.js
+++ Cyber4s/JS/GradesArry.js
@@ -1,7 +1,8 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
 let secondHalfGrades = grades.slice(Math.floor((grades.length)/2));
-secondHalfGrades.sort();
-grades.sort();
+secondHalfGrades.sort(function(a, b){return a-b});
+grades.sort(function(a, b){return a-b});
+console.log(grades);
 let lowestGrade = grades[0]
 let highestGrade = grades[grades.length-1];
 let differenceGrade = highestGrade - lowestGrade;
