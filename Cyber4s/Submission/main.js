06266b715faae3d230f8cae81948d12b0386a2af
ElayGelbart <elaygelbart@gmail.com>
2021-10-17 12:01:50 +0300

    Calc without webpack

--- Cyber4s/Submission/main.js
+++ Cyber4s/Submission/main.js
@@ -1,3 +1,45 @@
 import { add, sub, multiply, divide, equals } from './helpers/math.js';
 
-//// DOM ////
+/// select callback ///
+const selectCallbackAction = (ActionString) => {
+  if (ActionString === "/") {
+    return divide;
+  }
+  else if (ActionString === "*") {
+    return multiply;
+  }
+  else if (ActionString === "+") {
+    return add;
+  }
+  else if (ActionString === "-") {
+    return sub;
+  }
+}
+
+/// DOM ///
+const number1, number2, currentCallback;
+const action = (kindOfAction) => {
+  number1 = parseInt(document.getElementById("NumberInput").value);
+  currentCallback = selectCallbackAction(kindOfAction);
+  document.getElementById("NumberInput").value = '';
+}
+
+const sum = () => {
+  number2 = parseInt(document.getElementById("NumberInput").value);
+  document.getElementById("NumberInput").value = equals(number1, number2, currentCallback);
+}
+
+/// EventListeners ///
+document.getElementById("btnDivide").addEventListener("click", () => {
+  action("/")
+});
+document.getElementById("btnMultiply").addEventListener("click", () => {
+  action("*")
+});
+document.getElementById("btnMinus").addEventListener("click", () => {
+  action("-")
+});
+document.getElementById("btnPlus").addEventListener("click", () => {
+  action("+")
+});
+document.getElementById("btnEqual").addEventListener("click", sum);
