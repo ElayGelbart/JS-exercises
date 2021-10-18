ffdaf4de129bf4d87c077120028e156964eddad8
ElayGelbart <elaygelbart@gmail.com>
2021-10-18 19:43:26 +0300

    Final plus Bonus

--- Cyber4s/Submission/SearchTruesure.js
+++ Cyber4s/Submission/SearchTruesure.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const fsPromises = require('fs/promises');
 
 const mazeFolderDirString = `${__dirname}\\maze`;
 let mapTxtRoad = '';
 let lastDirUse = "yay";
 
 
-function findTreasureSync(roomPath) {
+async function findTreasureSync(roomPath) {
   if (/chest/.test(roomPath)) {
     console.log('chest');
     drawMapSync(roomPath);
@@ -15,7 +16,7 @@ function findTreasureSync(roomPath) {
   else if (/room-[\d]$/.test(roomPath)) {
     console.log(roomPath, "roomhere");
     try {
-      const chestUrl = fs.readFileSync(`${roomPath}\\chest-1.json`, 'utf8');
+      await fsPromises.readFile(`${roomPath}\\chest-1.json`, 'utf8');
       console.log("going in to clues");
       openChestSync(`${roomPath}\\chest-1.json`);
     } catch {
@@ -27,14 +28,19 @@ function findTreasureSync(roomPath) {
   }
 }
 
-function openChestSync(chestPath) {
+async function openChestSync(chestPath) {
   try {
-    const dataInsideChest = fs.readFileSync(chestPath, 'utf8');
+    const dataInsideChest = await fsPromises.readFile(chestPath, 'utf8');
     const chestObj = JSON.parse(dataInsideChest);
-    lastDirUse = chestPath;
+
     if (Object.keys(chestObj)[0] == 'clue') {
       console.log('made it to clues');
-      findTreasureSync(`${__dirname}\\${chestObj.clue.replace(/\//g, "\\")}\\chest-1.json`);
+      try {
+        fs.accessSync(Object.values(chestObj)[0]);
+        findTreasureSync(`${__dirname}\\${chestObj.clue.replace(/\//g, "\\")}\\chest-1.json`);
+      } catch (error) {
+        throw error
+      }
     }
     else if (Object.keys(chestObj)[0] == 'treasure') {
       drawMapSync("Treasure is Here");
@@ -46,10 +52,10 @@ function openChestSync(chestPath) {
   }
 }
 
-function drawMapSync(currentRoomPath) {
+async function drawMapSync(currentRoomPath) {
   mapTxtRoad += `${currentRoomPath.replace(__dirname, '')} \r\n`
   try {
-    const data = fs.writeFileSync(`${__dirname}\\map.txt`, mapTxtRoad);
+    const data = await fsPromises.writeFile(`${__dirname}\\map.txt`, mapTxtRoad);
     //file written successfully
   } catch (err) {
     console.error(err)
