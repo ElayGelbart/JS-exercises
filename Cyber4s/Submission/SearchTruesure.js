431eefcc8b2949a5fc6e78851d15a590bdaa7edb
ElayGelbart <elaygelbart@gmail.com>
2021-10-19 17:56:25 +0300

    Maze Runner v1.0
    async function of maze search !

--- Cyber4s/Submission/SearchTruesure.js
+++ Cyber4s/Submission/SearchTruesure.js
@@ -1,80 +1,44 @@
 const fs = require('fs');
 const path = require('path');
-// const fsPromises = require('fs/promises');
-const mazeFolderDirString = `${__dirname}\\maze`;
-let mapTxtRoad = '';
+const fsPromises = require('fs/promises');
+const mazeFolderDirString = `./maze`;
+let mapTxt = '';
 
-
-function findTruesureCB(roomPath, cb) {
-  if (/chest/.test(roomPath)) {
-    console.log('chest');
-    drawMap(roomPath, cb3);
-    openChest(roomPath, cb2);
-  }
-
-  else if (/room-[\d]$/.test(roomPath)) {
-    console.log(roomPath, "roomhere");
-    fs.readFile(`${roomPath}\\chest-1.json`, 'utf8', (err, data) => {
-      if (!err) {
-        console.log("going in to clues");
-        return openChest(`${roomPath}\\chest-1.json`, cb2);
-      }
-      findTruesureCB(`${roomPath}\\room-0`, cb);
+async function findTruesure(mazePath) {
+  drawMap(mazePath);
+  const dirArray = await fsPromises.readdir(mazePath);
+  for (let value of dirArray) {
+    if (/chest/.test(value)) {
+      openAsyncChest(`${mazePath}\\${value}`);
     }
-    );
-  }
-
-  else {
-    findTruesureCB(`${roomPath}\\room-0`, cb);
   }
 }
 
-function openChest(chestPath, cb) {
-  fs.readFile(chestPath, 'utf8', cb);
-}
-
-function drawMap(currentRoomPath, cb3) {
-  mapTxtRoad += `${currentRoomPath.replace(__dirname, '')} \r\n`
-  fs.writeFile(`${__dirname}\\map.txt`, mapTxtRoad, cb3);
-}
-
-const cb1 = (err, data) => { //cb
-  if (!err) {
-    console.log("going in to clues");
-    return openChest(`${roomPath}\\chest-1.json`, cb2);
-  }
-  return findTruesureCB(`${roomPath}\\room-0`, cb1);
-}
-
-const cb2 = (err, data) => {
-  if (!err) {
-    const chestObj = JSON.parse(data);
-    if (Object.keys(chestObj)[0] == 'clue') {
-      console.log('made it to clues');
-      fs.access(Object.values(chestObj)[0], (err, data) => {
-        if (!err) {
-          findTruesureCB(`${__dirname}\\${chestObj.clue.replace(/\//g, "\\")}\\chest-1.json`, cb1);
-        } else {
-          throw (err);
+async function openAsyncChest(chestPath) {
+  console.log('in chest open');
+  fsPromises.readFile(chestPath)
+    .then(value => {
+      try {
+        chestObj = JSON.parse(value);
+        if (Object.keys(chestObj) == 'clue') {
+          findTruesure(chestObj.clue);
         }
-      });
-    }
-    else if (Object.keys(chestObj)[0] == 'treasure') {
-      console.log("Found The Treasure");
-      return drawMap("Treasure is Here", cb3);
-    }
-
-  }
-  else {
-    console.log(chestPath, 'Failed ELAY ELAY ELAY')
-    console.error(err)
-  }
+        else if (Object.keys(chestObj) == 'treasure') {
+          console.log('Found treasure');
+          drawMap('Here is Gold');
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    })
+    .catch(err => {
+      console.error(err)
+    })
 }
 
-const cb3 = (err, data) => {
-  if (err) {
-    console.error(err)
-  }
+async function drawMap(currentPath) {
+  mapTxt += currentPath + '\r\n';
+  fsPromises.writeFile('./map.txt', mapTxt);
 }
 
-findTruesureCB(mazeFolderDirString, cb1);
+findTruesure(mazeFolderDirString);
