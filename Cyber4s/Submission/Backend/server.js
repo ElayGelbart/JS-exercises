b8dcee202a84c0064fa0c8efa2cc68ca09b8c525
ElayGelbart <elaygelbart@gmail.com>
2021-10-20 18:48:43 +0300

    Server now with POST

--- Cyber4s/Submission/Backend/server.js
+++ Cyber4s/Submission/Backend/server.js
@@ -3,10 +3,13 @@ const url = require('url');
 const validStudent = require("./db");
 
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {
-    'Access-Control-Allow-Origin': '*'
-  });
+const server = http.createServer(async (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  let stringOfBad = "";
+
   if (request.method == 'GET') {
     const current_url_params = new URL(`http://blahblah${request.url}`).searchParams
     const name = current_url_params.get("name");
@@ -22,11 +25,27 @@ const server = http.createServer((request, response) => {
     if (!validStudent.checkAbiliti(ability)) {
       stringOfBad += 'Bad Ability '
     }
-    response.write(stringOfBad);
-    response.end()
+    response.end();
   }
-});
+  request.on('data', chunk => {
+    let studentOBJ = JSON.parse(JSON.parse(chunk).studentObj);
+    if (!validStudent.checkName(studentOBJ.name)) {
+      stringOfBad += 'Bad Name ';
+    }
+    if (!validStudent.checkAge(studentOBJ.age)) {
+      stringOfBad += 'Bad Age ';
+      console.log(stringOfBad);
+    }
+    if (!validStudent.checkAbiliti(studentOBJ.ability)) {
+      stringOfBad += 'Bad Ability ';
+    }
+  });
+  request.on('end', () => {
+    console.log(stringOfBad);
+    response.end(stringOfBad);
 
+  });
+});
 server.listen(8080, (error) => {
   if (error) {
     console.log("error");
--- Cyber4s/Submission/Frontend/index.js
+++ Cyber4s/Submission/Frontend/index.js
@@ -5,7 +5,9 @@ submitBtn.addEventListener("click", async () => {
   const nameValue = document.getElementById("studentName").value;
   const ageValue = document.getElementById("studentAge").value;
   const abiltiValue = document.getElementById("studentAbilities").value;
-  const response = await axios.get(`http://localhost:8080?name=${nameValue}&age=${ageValue}&ability=${abiltiValue}`);
+  const studentObj = JSON.stringify({ 'name': nameValue, 'age': ageValue, 'ability': abiltiValue });
+  const response = await axios.post(`http://localhost:8080`, { studentObj });
+  console.log(response);
   if (response.data) {
     document.getElementById("passOrNot").innerHTML =
       `<div class="alert alert-danger" role="alert"> ${response.data} </div >`
