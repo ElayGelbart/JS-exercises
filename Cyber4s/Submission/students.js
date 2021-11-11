1a3120c25fefbe29f47e8ff560abe30054a68cd8
ElayGelbart <elaygelbart@gmail.com>
2021-11-11 19:04:47 +0200

    Yahalom Javascript

--- Cyber4s/Submission/students.js
+++ Cyber4s/Submission/students.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const faker = require('faker');
+const { $regex } = require("sift");
 
 const mongoPass = process.argv[2];
 const mongoUrl = `mongodb+srv://elaygelbart:${mongoPass}@elaygelbart.qhmbq.mongodb.net/Ofer?retryWrites=true&w=majority`;
@@ -43,11 +44,64 @@ const insertFakerStudents = async () => {
   mongoose.connection.close();
 }
 //  inserting Fake Information to DB;
-insertFakerStudents();
+// insertFakerStudents();
 
 const getAllStudents = async () => {
   const allStud = await StudentModel.find({});
   console.log(allStud);
+  mongoose.connection.close();
 }
 // get all Students
 // getAllStudents();
+
+const getRobertsStudent = async () => {
+  const Idos = await StudentModel.find({ name: "Robert" })
+  console.log(Idos);
+  mongoose.connection.close();
+}
+// get Roberts because Faker
+// getRobertsStudent();
+
+const getLawCoursesStudents = async () => {
+  const lawStudents = await StudentModel.find({ courses: { $elemMatch: { $eq: 'Frozen' } } })
+  console.log(lawStudents);
+  mongoose.connection.close();
+}
+// get all law Student, but Frozen Because Faker
+// getLawCoursesStudents();
+
+const getFemaleJaveStudents = async () => {
+  const femaleJaveStudents = await StudentModel.find({ courses: { $elemMatch: { $eq: 'Frozen' } }, gender: 'Cis' })
+  console.log(femaleJaveStudents);
+  mongoose.connection.close();
+}
+// get all femaleJavaStudents, but Frozen and Cis Gender Because Faker
+// getFemaleJaveStudents();
+
+const get1998greaterStudents = async () => {
+  const oldStudents = await StudentModel.find({ birth: { $gt: new Date("1998/05/05") } });
+  console.log(oldStudents);
+  mongoose.connection.close();
+}
+// get 1998 greater Students
+// get1998greaterStudents();
+
+const getPhone054Students = async () => {
+  const phone054Students = await StudentModel.find({ phone: { $regex: /^542/ } });
+  console.log(phone054Students);
+  mongoose.connection.close();
+}
+//get 054 Students Phone Obj but start with 542 because Faker
+// getPhone054Students();
+
+const updateYahalomStudentToJS = async () => {
+  await StudentModel.updateMany({ name: "Jaren" }, { $addToSet: { courses: "JavaScript" } });
+  mongoose.connection.close();
+}
+// Update JS TO JARAEN BECAUSE FAKER
+// updateYahalomStudentToJS();
+
+
+
+
+
